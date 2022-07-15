27e62b4a5f751d5cabb66c7853277d4fc8526263
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-07-15 11:13:13 +0200

    test: added test for dataloaders

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,9 @@
 import pytest
-from snip.cli.convert import convert
 from typer.testing import CliRunner
 
-from .test_utils import bed_path, zarr_tmp_path  # noqa
+from snip.cli.convert import convert
+
+from .utils import bed_path, zarr_tmp_path  # noqa
 
 runner = CliRunner()
 
@@ -21,12 +22,25 @@ def test_convert(bed_path, zarr_tmp_path):  # noqa
     assert zarr_tmp_path.is_dir()
 
 
-# def test_cli_convert(app, bed_path, zarr_tmp_path):  # noqa
-#     result = runner.invoke(app, ["convert", f"{bed_path}", f"{zarr_tmp_path}"])
-#     print(bed_path)
-#     print(zarr_tmp_path)
-#     import os
+def test_cli_convert(app, bed_path, zarr_tmp_path):  # noqa
+    result = runner.invoke(app, ["convert", f"{bed_path}", f"{zarr_tmp_path}"])
+    print(bed_path)
+    print(zarr_tmp_path)
+    import os
 
-#     print(os.listdir())
-#     assert result.exit_code == 0
-#     assert zarr_tmp_path.is_dir()
+    print(os.listdir())
+    assert result.exit_code == 0
+    assert zarr_tmp_path.is_dir()
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests(zarr_tmp_path):  # noqa
+    # Code that will run before your test, for example:
+    # if test data is empty create it
+
+    # A test function will be run at this point
+    yield
+    # clean up test files
+    import shutil
+
+    shutil.rmtree(zarr_tmp_path)
