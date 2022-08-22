c0b25c3e62d41e8723d8a8ce1bfbd4bf74e9b170
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-08-22 18:34:22 +0200

    refactor: Remove unused model code

--- src/snip/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .create import create_model  # noqa
-from .denoising_autoencoder import DenoisingAutoencoder  # noqa
-from .pl_wrapper import PlOnehotWrapper  # noqa
-from .snpnet import SNPDecoder, SNPEncoder  # noqa
