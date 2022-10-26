00225f0a6b587fef9cec49ea0f1ae4701b8618ab
KennethEnevoldsen <kennethcenevoldsen@gmail.com>
2022-10-26 14:24:11 +0200

    Updated train test split

---
+++ project/002_train_test_split.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --mem 64g
+#SBATCH -c 16
+#SBATCH --output ./project/reports/slurm-output/%x-%u-%j.out
+#SBATCH -A NLPPred
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=slurm-notifications-aaaahkuvjxiplokhffpn6qphzu@chcaa.slack.com
+ 
+python /home/kce/NLPPred/github/snip/project/002_train_test_split.py
