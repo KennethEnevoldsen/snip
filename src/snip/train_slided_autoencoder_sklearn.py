5c2a9a21daadb048f9c90ad99705e29589390119
KennethEnevoldsen <kennethcenevoldsen@gmail.com>
2022-10-28 13:07:40 +0200

    implemented reconstruction loss (avoided saving interim files)

--- src/snip/train_slided_autoencoder_sklearn.py
+++ src/snip/train_slided_autoencoder_sklearn.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from itertools import repeat
 from multiprocessing import Pool
 from pathlib import Path
-from typing import Optional, Sequence, Tuple
+from typing import Dict, Optional, Sequence, Tuple
 
 import dask
 import hydra
@@ -25,6 +25,20 @@ CONFIG_PATH = Path(__file__).parent / "configs"
 # speeds up data loading
 # https://discuss.pytorch.org/t/problems-using-dataloader-for-dask-xarray-netcdf-data/108270/3
 dask.config.set(scheduler="synchronous")
+
+
+def create_autoencoder(cfg: Namespace) -> MLPRegressor:
+    """Create a autoencoder."""
+
+    if cfg.model.architecture == "mlp":
+        model = MLPRegressor(
+            hidden_layer_sizes=cfg.model.hidden_layer_sizes,
+            activation=cfg.model.activation,
+            solver=cfg.model.solver,
+        )
+    else:
+        raise NotImplementedError("Model not implemented")
+    return model
 
 
 def create_encoder(clf):
@@ -56,18 +70,33 @@ def create_encoder(clf):
     return encoder
 
 
-def create_autoencoder(cfg: Namespace) -> MLPRegressor:
-    """Create a autoencoder."""
+def create_decoder(clf):
+    """Create a decoder from a sklearn MLPRegressor."""
 
-    if cfg.model.architecture == "mlp":
-        model = MLPRegressor(
-            hidden_layer_sizes=cfg.model.hidden_layer_sizes,
-            activation=cfg.model.activation,
-            solver=cfg.model.solver,
-        )
-    else:
-        raise NotImplementedError("Model not implemented")
-    return model
+    def decoder(X):
+        # find the bottleneck in layers
+        bottleneck = np.argmin([layer.shape[1] for layer in clf.coefs_])
+        # extract activation function
+        if clf.activation == "relu":
+
+            def activation(x):
+                return np.maximum(0, x)
+
+        elif clf.activation == "identity":
+
+            def activation(x):
+                return x
+
+        for weight, bias in zip(
+            clf.coefs_[bottleneck + 1 :],
+            clf.intercepts_[bottleneck + 1 :],
+        ):
+            X = X @ weight + bias
+            # activation
+            X = activation(X)
+        return X
+
+    return decoder
 
 
 def create_datasets(
@@ -130,7 +159,7 @@ def train(
         n: The index of the dataset split.
         cfg: The configuration.
     """
-    interim_path = Path(cfg.data.interim_path) / wandb.config.run_name
+    # interim_path = Path(cfg.data.interim_path) / wandb.config.run_name
 
     model = create_autoencoder(cfg)
     msg.info(f"Training autoencoder {n}")
@@ -146,19 +175,29 @@ def train(
     # # apply
     encoder = create_encoder(model)
     c_snps = encoder(train_X)
+    decoder = create_decoder(model)
+    yhat = decoder(c_snps)
+    # calculate reconstruction error
+    train_reconstruction_error = np.mean((train_X - yhat) ** 2)
+
     c_train = PLINKIterableDataset.from_array(c_snps, metadata_from=train)
-    c_train.to_disk(interim_path / f"train_{n}.zarr", mode="w")
+    # c_train.to_disk(interim_path / f"train_{n}.zarr", mode="w")
     # apply val
     validation_X = validation.get_X().compute()
     c_snps = encoder(validation_X)
     c_val = PLINKIterableDataset.from_array(c_snps, metadata_from=validation)
-    c_val.to_disk(interim_path / f"validation_{n}.zarr", mode="w")
+    yhat = decoder(c_snps)
+    # calculate reconstruction error
+    val_reconstruction_error = np.mean((train_X - yhat) ** 2)
+
+    # c_val.to_disk(interim_path / f"validation_{n}.zarr", mode="w")
     # apply test
     if test:
         test_X = test.get_X().compute()
         c_snps = encoder(test_X)
         c_test = PLINKIterableDataset.from_array(c_snps, metadata_from=test)
-        c_test.to_disk(interim_path / f"test_{n}.zarr", mode="w")
+        # c_test.to_disk(interim_path / f"test_{n}.zarr", mode="w")
+    return c_train, c_val, c_test, train_reconstruction_error, val_reconstruction_error
 
 
 @hydra.main(
@@ -192,17 +231,41 @@ def main(cfg: DictConfig) -> None:
 
     # train the local MLPs in parallel
     with Pool(cfg.project.n_jobs) as pool:
-        pool.starmap(
+        results = pool.starmap(
             train,
             zip(dataset_splits, range(len(datasets[0])), repeat(cfg)),
         )
+
+    # log avg, min, max reconstruction errors
+    train_reconstruction_errors = [r[3] for r in results]
+    val_reconstruction_errors = [r[4] for r in results]
+    wandb.log(
+        {
+            "Mean(training reconstruction error)": np.mean(train_reconstruction_errors),
+            "Mean(valilidation reconstruction error)": np.mean(
+                val_reconstruction_errors,
+            ),
+            "Min(training reconstruction error)": np.min(train_reconstruction_errors),
+            "Min(validation reconstruction error)": np.min(val_reconstruction_errors),
+            "Max(training reconstruction error)": np.max(train_reconstruction_errors),
+            "Max(validation reconstruction error)": np.max(val_reconstruction_errors),
+        },
+    )
+
+    # convert reuslts to dict
+    compressions: Dict[str, list] = {"train": [], "validation": [], "test": []}
+    for result in results:
+        compressions["train"].append(result[0])
+        compressions["validation"].append(result[1])
+        compressions["test"].append(result[2])
 
     # merge apply files
     for split in ["train", "validation", "test"]:
         if split == "test" and not cfg.data.test_path:
             continue
-        compressed_snps_paths = list(interim_path.glob(f"{split}_*.zarr"))
-        genotypes = [PLINKIterableDataset(path) for path in compressed_snps_paths]
+        # compressed_snps_paths = list(interim_path.glob(f"{split}_*.zarr"))
+        # genotypes = [PLINKIterableDataset(path) for path in compressed_snps_paths]
+        genotypes = compressions[split]
 
         if cfg.project.verbose:
             msg.info(f"Merging {split}, consisting of {len(genotypes)} files")
